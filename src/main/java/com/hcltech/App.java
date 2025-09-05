56996fc5d23aeea78c321154325da4f822ea8e80
Saravana Kumar M <kumar9885035454@gmail.com>
2025-09-05 11:29:51 +0530

    Refactored the App class.

--- src/main/java/com/hcltech/App.java
+++ src/main/java/com/hcltech/App.java
@@ -3,7 +3,8 @@ package com.hcltech;
 public class App {
 
     public static void main(String[] args) {
-        Greeting greeting = new Greeting();
-        System.out.println(greeting.getMessage());
+        Greeting     greeting = new Greeting();
+        final String message  = greeting.getMessage();
+        System.out.println(message);
     }
 }
